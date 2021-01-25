ff9ac7ec0180439e6c919305dbd956f616e1b515
Supreet <supreet.kaur@live.com>
2021-01-25 16:50:14 -0500

    Serve side validation for user email,pwd

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,11 @@
 module.exports = {
     signUp: async (req, res, next) => {
+        //Validate Email and Password
+        console.log('contents of req.value,body', req.value.body);
         console.log('UsersController.signUp() called!');    
     },
     signIn: async (req, res, next) => {
+        //Generate token
         console.log('UsersController.signIn() called!');    
     },
     secret: async (req, res, next) => {
