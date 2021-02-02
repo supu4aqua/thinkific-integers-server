42602041e7dd54eb4fdf8a14a3d3d7f2eb96ac9f
Supreet <supreet.kaur@live.com>
2021-02-02 13:58:37 -0500

    Mongo hoisting

--- server/app.js
+++ server/app.js
@@ -3,13 +3,26 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const mongoPath = 'mongodb+srv://mongo-users:2pF8CcARdXybOKaJ@thinkific-users-mongodb.pw95m.mongodb.net/<dbname>?retryWrites=true&w=majority'
-
 require("dotenv").config();
+const mongoPath = //process.env.DATABASE_URL;
+  'mongodb://mongo-users:2pF8CcARdXybOKaJ@thinkific-users-mongodb-shard-00-00.pw95m.mongodb.net:27017,thinkific-users-mongodb-shard-00-01.pw95m.mongodb.net:27017,thinkific-users-mongodb-shard-00-02.pw95m.mongodb.net:27017/UserAuthentication?ssl=true&replicaSet=atlas-3ztqu7-shard-0&authSource=admin&retryWrites=true&w=majority'
+  
+//'mongodb+srv://mongo-users:2pF8CcARdXybOKaJ@thinkific-users-mongodb.pw95m.mongodb.net/ThinkificUsers?retryWrites=true&w=majority&ssl=true'
 
-mongoose.Promise = global.Promise;
-mongoose.connect(mongoPath, { useNewUrlParser: true , useUnifiedTopology: true});
 
+
+//mongoose.Promise = global.Promise;
+try {
+  mongoose.connect(mongoPath, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log('Connection successful');
+} catch (error) { handleError(error); }
+  /*  .then(() => {
+        console.log('start');
+    })
+    .catch(err => {
+        console.error('App starting error:', err.stack);
+        process.exit(1)
+    }); */
 
 const app = express();
 //app.use(cors());
--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
 const app = require('./app');
 
+/*const connectToMongoDB = async () => {
+    await app().then((mongoose) => {
+        try {
+            console.log('Connected to mongodb!')
+        } finally {
+            mongoose.connection.close()
+        }
+    })
+}
+
+connectToMongoDB()*/
+
 // Start the server
 const port = process.env.PORT || 8000;
 app.listen(port);
-console.log(`Server listening at ${port}`);
+console.log(`Server listening at ${port}`)
