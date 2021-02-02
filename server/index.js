1f1dc5a1e83fda644a253d490f6c07cc2b7b600c
Supreet <supreet.kaur@live.com>
2021-02-02 15:09:02 -0500

    dotenv updated

--- server/index.js
+++ server/index.js
@@ -1,17 +1,5 @@
 const app = require('./app');
 
-/*const connectToMongoDB = async () => {
-    await app().then((mongoose) => {
-        try {
-            console.log('Connected to mongodb!')
-        } finally {
-            mongoose.connection.close()
-        }
-    })
-}
-
-connectToMongoDB()*/
-
 // Start the server
 const port = process.env.PORT || 8000;
 app.listen(port);
